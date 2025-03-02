84d85a0fd2e3538055d02c9df7d7343412d80e76
Muhammad Hunfa Jalil <83254423+hunfa@users.noreply.github.com>
2025-03-02 20:55:22 +0500

    add template link

--- src/@core/layouts/components/tables/columns/templatesTableColumns.tsx
+++ src/@core/layouts/components/tables/columns/templatesTableColumns.tsx
@@ -1,4 +1,4 @@
-import { Select, MenuItem } from '@mui/material'
+import { Select, MenuItem, Button } from '@mui/material'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 import { useState } from 'react'
@@ -58,7 +58,18 @@ export const TemplateColumns: any = () => [
   },
   {
     header: 'Template Link',
-    accessorKey: 'details.home.name',
-    Cell: ({ cell }: any) => cell.getValue() || 'N/A'
+    accessorKey: '_id',
+    Cell: ({ cell }: any) => {
+      return (
+        <Button
+          variant='contained'
+          onClick={() => {
+            window.open(`https://diy-platform.vercel.app/templete/${cell.getValue()}`, '_blank')
+          }}
+        >
+          View Template
+        </Button>
+      )
+    }
   }
 ]
